e15f0fe38b57e982c3ad3e1cc2e3690137960dfe
TatsHats <6642017@gmail.com>
2024-10-01 14:00:05 +0300

    feat: start js file and add event listener and snake (Вт, окт 01, 2024 02:00:05 )

--- random-game/game.js
+++ random-game/game.js
@@ -1,14 +1,17 @@
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
-let box = 32;
+let square = 32;
 let score = 0;
+
+let food = {
+    x: Math.floor(Math.random() * 17 + 1) * square,
+    y: Math.floor(Math.random() * 15 + 3) * square
+};
 
 const background = new Image();
 background.src = './img/background.png';
-
-background.onload = function() {
-    drawGame();
-};
+const imgScore = new Image();
+imgScore.src = './img/scoreImg.png';
 
 const foodImages = [
     './img/apple.png',
@@ -23,11 +26,61 @@ function getRandomFoodImage() {
     return foodImages[randomIndex];
 }
 
-const food = new Image();
-food.src = getRandomFoodImage();
+const foodImage = new Image();
+foodImage.src = getRandomFoodImage();
 
 function drawGame() {
     ctx.drawImage(background, 0, 0);
+    ctx.drawImage(foodImage, food.x, food.y);
+    ctx.drawImage(imgScore, square, square);
+
+    for(let i = 0; i < snake.length; i++) {
+        ctx.fillStyle = 'green';
+        ctx.fillRect(snake[i].x, snake[i].y, square, square);
+    }
+
+    ctx.fillStyle = 'white';
+    ctx.font = '40px Arial';
+    ctx.fillText(score, square * 2.5, square * 2);
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    snake.pop();
+
+    if(dir === 'left') snakeX -= square;
+    if(dir === 'right') snakeX += square;
+    if(dir === 'up') snakeY -= square;
+    if(dir === 'down') snakeY += square;
+
+    let snakeHead = {
+        x: snakeX,
+        y: snakeY
+    };
+
+    snake.unshift(snakeHead);
 }
 
+
+
 let game = setInterval(drawGame, 100);
+
+let snake = [];
+snake[0] = {
+    x: 9 * square,
+    y: 10 * square
+};
+
+//обработчик событий для передвижения змейки
+document.addEventListener('keydown', direction);
+let dir;
+function direction(ev) {
+    if(ev.keyCode === 37 && dir != 'right')
+        dir = 'left';
+    else if(ev.keyCode === 38 && dir != 'down')
+        dir = 'up';
+    else if(ev.keyCode === 39 && dir != 'left')
+        dir = 'right';
+    else if(ev.keyCode === 40 && dir != 'up')
+        dir = 'down';
+}
